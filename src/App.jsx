5353042abcbc1da1f0d2d6f2ce0a4404f09f4e7e
Cryptic <mothoagae.ks@gmail.com>
2024-08-12 00:19:11 +0200

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-import Home from './pages/Home.jsx';
-import Contact from './pages/Contact.jsx';
-// import About from "./pages";
+import {
+    HashRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages";
+import Cart from "./pages/cart";
+import ShoppingPage from "./pages/shopping";
+import {Provider} from "react-redux";
+import store from "./store";
+
 function App() {
     return (
-        <Router>
-            <div>
-                <title>MMMMMMMMM</title>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/contact" component={Contact} />
-                </Switch>
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/shopping" element={<ShoppingPage />} />
+                </Routes>
+            </Router>
+        </Provider>
     );
 }
 
 export default App;
+
